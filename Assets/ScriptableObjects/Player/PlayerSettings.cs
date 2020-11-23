b01997ff2cdc5b79be5721c9af9162b99c8596cf
leochocolat <l.mouraire@outlook.fr>
2020-11-23 22:47:19 +0100

    :sparkles: Player Movement > Add sets of settings for different movement modes

---
+++ Assets/ScriptableObjects/Player/PlayerSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerSettings", menuName = "ScriptableObjects/PlayerSettings")]
+public class PlayerSettings : ScriptableObject
+{
+    public float speed = 10f;
+    public float rotationSpeed = 5f;
+    public float headBobAmplitude = 0.5f;
+    public float headBobSpeed = 0.1f;
+}
