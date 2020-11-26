ece740b7a250427c1fe0cf4a8bfd1dbdfed3d031
leochocolat <l.mouraire@outlook.fr>
2020-11-26 17:02:50 +0100

    :sparkles: Cinematic > Add timeline animation with player animations and camera movements

---
+++ Assets/Scripts/CustomPathFollower.cs
@@ -0,0 +1,28 @@
+﻿#pragma warning disable 0649
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class CustomPathFollower : MonoBehaviour
+{
+    [SerializeField]
+    private PathCreator _pathCreator;
+
+    [SerializeField]
+    private float _speed;
+
+    private float progress = 0f;
+
+    void Update()
+    {
+        progress += _speed * Time.deltaTime;
+
+        Vector3 position = _pathCreator.path.GetPointAtTime(progress);
+        Quaternion rotation = _pathCreator.path.GetRotation(progress);
+
+        transform.position = position;
+        transform.rotation = rotation;
+    }
+}
