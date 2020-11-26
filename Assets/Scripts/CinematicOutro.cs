7f64419259905575066fd6f43e9c770154b9f696
leochocolat <l.mouraire@outlook.fr>
2020-11-26 23:35:35 +0100

    :sparkles: Cinematic Outro > Setup Basic post processing effect

---
+++ Assets/Scripts/CinematicOutro.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class CinematicOutro : MonoBehaviour
+{
+    [SerializeField]
+    private Volume _volume;
+    private ColorAdjustments _colorAdjustments;
+
+    private void Awake()
+    {
+        _volume.profile.TryGet(out _colorAdjustments);
+        // _colorAdjustments.postExposure.value = -10f;
+    }
+}
