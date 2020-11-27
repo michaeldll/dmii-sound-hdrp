e2f98f13ce833a2d9d083feb63e7aff6d8f2bc6f
leochocolat <l.mouraire@outlook.fr>
2020-11-27 11:21:08 +0100

    :sparkles: Cinematic Outro > Add Timeline to animate exposure

--- Assets/Scripts/CinematicOutro.cs
+++ Assets/Scripts/CinematicOutro.cs
@@ -4,16 +4,47 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.Experimental.Rendering.Universal;
+using DG.Tweening;
+using UnityEngine.Playables;
 
 public class CinematicOutro : MonoBehaviour
 {
     [SerializeField]
+    private float _colorExposure;
+
+    [SerializeField]
     private Volume _volume;
+
+    [SerializeField]
+    private PlayableDirector _director;
+
     private ColorAdjustments _colorAdjustments;
 
-    private void Awake()
+    public void FadeOut()
+    {
+        _director.Play();
+    }
+
+    // Hook
+    void Awake()
     {
         _volume.profile.TryGet(out _colorAdjustments);
-        // _colorAdjustments.postExposure.value = -10f;
+        _colorAdjustments.postExposure.value = 0f;
+    }
+
+    void Start()
+    {
+        FadeOut();
+    }
+
+    void Update() {
+        _colorAdjustments.postExposure.value = _colorExposure;
+    }
+
+    void OnDrawGizmos() {
+        if (_colorAdjustments)
+        {
+            _colorAdjustments.postExposure.value = _colorExposure;
+        }
     }
 }
