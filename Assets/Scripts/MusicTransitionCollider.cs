b19ef47c05dac4c85980a283965e771b2331b534
dsadsa <ac2031@hotmail.com>
2020-11-27 02:19:48 +0100

    :sparkles: Add music, add music transition trigger

---
+++ Assets/Scripts/MusicTransitionCollider.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class MusicTransitionCollider : MonoBehaviour
+{
+    [SerializeField]
+    AudioMixerSnapshot snapshotActive;
+
+    [SerializeField]
+    AudioMixerSnapshot snapshotInactive;
+
+    [SerializeField]
+    AudioMixer mixer;
+
+    [SerializeField]
+    float transitionTime = 2.0f;
+
+    public TransitionType type = TransitionType.ToActive;
+    public enum TransitionType
+    {
+        ToActive,
+        ToInactive
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if(type == TransitionType.ToActive) snapshotActive.TransitionTo(transitionTime);
+        else if (type == TransitionType.ToInactive) snapshotInactive.TransitionTo(transitionTime);
+    }
+}
