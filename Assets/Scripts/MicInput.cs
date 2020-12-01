ab33740d835098a7843a1a78173c314d4d2f4339
dsadsa <ac2031@hotmail.com>
2020-12-01 21:08:07 +0100

    Various tweaks

--- Assets/Scripts/MicInput.cs
+++ Assets/Scripts/MicInput.cs
@@ -34,165 +34,165 @@ public class MicInput : MonoBehaviour
     private bool _isInitialized;
 
     //mic initialization
-    public void InitMic()
-    {
-        foreach (string micDevice in Microphone.devices)
-        {
-            allDevices.Add(micDevice);
-        }
+    // public void InitMic()
+    // {
+    //     foreach (string micDevice in Microphone.devices)
+    //     {
+    //         allDevices.Add(micDevice);
+    //     }
 
-        if (device == null)
-        {
-            device = Microphone.devices[0];
-        }
-        _clipRecord = Microphone.Start(device, true, 999, 44100);
-        _isInitialized = true;
-    }
+    //     if (device == null)
+    //     {
+    //         device = Microphone.devices[0];
+    //     }
+    //     _clipRecord = Microphone.Start(device, true, 999, 44100);
+    //     _isInitialized = true;
+    // }
 
-    public void StopMicrophone()
-    {
-        Microphone.End(device);
-        _isInitialized = false;
-    }
+    // public void StopMicrophone()
+    // {
+    //     Microphone.End(device);
+    //     _isInitialized = false;
+    // }
 
-    //get data from microphone into audioclip
-    float MicrophoneLevelMax()
-    {
-        float levelMax = 0;
-        float[] waveData = new float[_sampleWindow];
-        int micPosition = Microphone.GetPosition(null) - (_sampleWindow + 1); // null means the first microphone
-        if (micPosition < 0) return 0;
-        _clipRecord.GetData(waveData, micPosition);
-        // Getting a peak on the last 128 samples
-        for (int i = 0; i < _sampleWindow; i++)
-        {
-            float wavePeak = waveData[i] * waveData[i];
-            if (levelMax < wavePeak)
-            {
-                levelMax = wavePeak;
-            }
-        }
-        return levelMax;
-    }
+    // //get data from microphone into audioclip
+    // float MicrophoneLevelMax()
+    // {
+    //     float levelMax = 0;
+    //     float[] waveData = new float[_sampleWindow];
+    //     int micPosition = Microphone.GetPosition(null) - (_sampleWindow + 1); // null means the first microphone
+    //     if (micPosition < 0) return 0;
+    //     _clipRecord.GetData(waveData, micPosition);
+    //     // Getting a peak on the last 128 samples
+    //     for (int i = 0; i < _sampleWindow; i++)
+    //     {
+    //         float wavePeak = waveData[i] * waveData[i];
+    //         if (levelMax < wavePeak)
+    //         {
+    //             levelMax = wavePeak;
+    //         }
+    //     }
+    //     return levelMax;
+    // }
 
-    //get data from microphone into audioclip
-    float MicrophoneLevelMaxDecibels()
-    {
+    // //get data from microphone into audioclip
+    // float MicrophoneLevelMaxDecibels()
+    // {
 
-        float db = 20 * Mathf.Log10(Mathf.Abs(_micLoudness));
+    //     float db = 20 * Mathf.Log10(Mathf.Abs(_micLoudness));
 
-        return db;
-    }
+    //     return db;
+    // }
 
-    public float FloatLinearOfClip(AudioClip clip)
-    {
-        StopMicrophone();
-
-        _recordedClip = clip;
-
-        float levelMax = 0;
-        float[] waveData = new float[_recordedClip.samples];
-
-        _recordedClip.GetData(waveData, 0);
-        // Getting a peak on the last 128 samples
-        for (int i = 0; i < _recordedClip.samples; i++)
-        {
-            float wavePeak = waveData[i] * waveData[i];
-            if (levelMax < wavePeak)
-            {
-                levelMax = wavePeak;
-            }
-        }
-        return levelMax;
-    }
-
-    public float DecibelsOfClip(AudioClip clip)
-    {
-        StopMicrophone();
-
-        _recordedClip = clip;
-
-        float levelMax = 0;
-        float[] waveData = new float[_recordedClip.samples];
-
-        _recordedClip.GetData(waveData, 0);
-        // Getting a peak on the last 128 samples
-        for (int i = 0; i < _recordedClip.samples; i++)
-        {
-            float wavePeak = waveData[i] * waveData[i];
-            if (levelMax < wavePeak)
-            {
-                levelMax = wavePeak;
-            }
-        }
-
-        float db = 20 * Mathf.Log10(Mathf.Abs(levelMax));
-
-        return db;
-    }
-
-
-
-    void Update()
-    {
-        // levelMax equals to the highest normalized value power 2, a small number because < 1
-        // pass the value to a static var so we can access it from anywhere
-        _micLoudness = MicrophoneLevelMax();
-        micLoudnessinDecibels = MicrophoneLevelMaxDecibels();
-        if (micLoudnessinDecibels > minimumLimitDb)
-        {
-            micNormalized = Mathf.Clamp((1 / Mathf.Abs(micLoudnessinDecibels) * amplitude), 0, 1);
-            data.SetVolume(micNormalized);
-        }
-        else
-        {
-            data.SetVolume(0f);
-        }
-    }
-
-
-
-    // start mic when scene starts
-    void OnEnable()
-    {
-        InitMic();
-        _isInitialized = true;
-    }
-
-    //stop mic when loading a new level or quit application
-    // void OnDisable()
+    // public float FloatLinearOfClip(AudioClip clip)
     // {
     //     StopMicrophone();
-    // }
- 
 
-    // void OnDestroy()
+    //     _recordedClip = clip;
+
+    //     float levelMax = 0;
+    //     float[] waveData = new float[_recordedClip.samples];
+
+    //     _recordedClip.GetData(waveData, 0);
+    //     // Getting a peak on the last 128 samples
+    //     for (int i = 0; i < _recordedClip.samples; i++)
+    //     {
+    //         float wavePeak = waveData[i] * waveData[i];
+    //         if (levelMax < wavePeak)
+    //         {
+    //             levelMax = wavePeak;
+    //         }
+    //     }
+    //     return levelMax;
+    // }
+
+    // public float DecibelsOfClip(AudioClip clip)
     // {
     //     StopMicrophone();
+
+    //     _recordedClip = clip;
+
+    //     float levelMax = 0;
+    //     float[] waveData = new float[_recordedClip.samples];
+
+    //     _recordedClip.GetData(waveData, 0);
+    //     // Getting a peak on the last 128 samples
+    //     for (int i = 0; i < _recordedClip.samples; i++)
+    //     {
+    //         float wavePeak = waveData[i] * waveData[i];
+    //         if (levelMax < wavePeak)
+    //         {
+    //             levelMax = wavePeak;
+    //         }
+    //     }
+
+    //     float db = 20 * Mathf.Log10(Mathf.Abs(levelMax));
+
+    //     return db;
     // }
+
+
+
+    // void Update()
+    // {
+    //     // levelMax equals to the highest normalized value power 2, a small number because < 1
+    //     // pass the value to a static var so we can access it from anywhere
+    //     _micLoudness = MicrophoneLevelMax();
+    //     micLoudnessinDecibels = MicrophoneLevelMaxDecibels();
+    //     if (micLoudnessinDecibels > minimumLimitDb)
+    //     {
+    //         micNormalized = Mathf.Clamp((1 / Mathf.Abs(micLoudnessinDecibels) * amplitude), 0, 1);
+    //         data.SetVolume(micNormalized);
+    //     }
+    //     else
+    //     {
+    //         data.SetVolume(0f);
+    //     }
+    // }
+
+
+
+    // // start mic when scene starts
+    // void OnEnable()
+    // {
+    //     InitMic();
+    //     _isInitialized = true;
+    // }
+
+    // //stop mic when loading a new level or quit application
+    // // void OnDisable()
+    // // {
+    // //     StopMicrophone();
+    // // }
+ 
+
+    // // void OnDestroy()
+    // // {
+    // //     StopMicrophone();
+    // // }
  
  
 
 
-    // make sure the mic gets started & stopped when application gets focused
-    void OnApplicationFocus(bool focus)
-    {
-        if (focus)
-        {
-            //Debug.Log("Focus");
+    // // make sure the mic gets started & stopped when application gets focused
+    // void OnApplicationFocus(bool focus)
+    // {
+    //     if (focus)
+    //     {
+    //         //Debug.Log("Focus");
 
-            if (!_isInitialized)
-            {
-                //Debug.Log("Init Mic");
-                InitMic();
-            }
-        }
-        if (!focus)
-        {
-            //Debug.Log("Pause");
-            // StopMicrophone();
-            //Debug.Log("Stop Mic");
+    //         if (!_isInitialized)
+    //         {
+    //             //Debug.Log("Init Mic");
+    //             InitMic();
+    //         }
+    //     }
+    //     if (!focus)
+    //     {
+    //         //Debug.Log("Pause");
+    //         // StopMicrophone();
+    //         //Debug.Log("Stop Mic");
 
-        }
-    }
+        // }
+    // }
 }
